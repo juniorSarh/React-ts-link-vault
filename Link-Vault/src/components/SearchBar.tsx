38e3045f54f2458fe560679713b45d730caef3a2
JuniorSarh <sabelohgumede@gmail.com>
2025-09-11 14:26:51 +0200

    feat(navbar): add responsive media queries and fix layout on tablets/phones

--- Link-Vault/src/components/SearchBar.tsx
+++ Link-Vault/src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from "react";
 import type { ChangeEvent, KeyboardEvent } from "react";
-import searchIcon from "../Icons/search.png";
+
 
 type SearchBarProps = {
   value: string;
